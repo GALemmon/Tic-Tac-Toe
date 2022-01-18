b6f6cc6d6629875262dac07554e22bade422f1dd
George Lemmon <george.lemmon@gmail.com>
2022-01-18 12:18:21 -0700

    Add assets and js functionality

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
+
 //------------ Query Selectors -------------
 var gameArea = document.querySelector('.game-area');
+var player1WinCounter = document.getElementById('player1WinCounter');
+var player2WinCounter = document.getElementById('player2WinCounter');
+var gameResult = document.getElementById('result');
+
+//--------------- Buttons -------------------
+var gameSquareBtns = document.querySelector('.game-grid');
+var buttons = document.getElementsByTagName('button');
 var square1 = document.getElementById('square1');
 var square2 = document.getElementById('square2');
 var square3 = document.getElementById('square3');
@@ -9,55 +17,61 @@ var square6 = document.getElementById('square6');
 var square7 = document.getElementById('square7');
 var square8 = document.getElementById('square8');
 var square9 = document.getElementById('square9');
-var player1WinCounter = document.getElementById('player1WinCounter');
-var player2WinCounter = document.getElementById('player2WinCounter');
+
 
 //----------- Global Variables --------------
-var gameSquares = [square1, square2, square3, square4, square5, square6,
-   square7, square8, square9];
+var gameSquares = [square1, square2, square3, square4, square5, square6, square7, square8, square9]
 var winningCombos = [
   ['square1', 'square2', 'square3'],
-  ['square3', 'square2', 'square1'],
   ['square4', 'square5', 'square6'],
-  ['square6', 'square5', 'square4'],
   ['square7', 'square8', 'square9'],
-  ['square9', 'square8', 'square7'],
   ['square1', 'square4', 'square7'],
-  ['square7', 'square4', 'square1'],
   ['square2', 'square5', 'square8'],
-  ['square8', 'square5', 'square2'],
   ['square3', 'square6', 'square9'],
-  ['square9', 'square6', 'square3'],
   ['square1', 'square5', 'square9'],
-  ['square9', 'square5', 'square1'],
   ['square3', 'square5', 'square7'],
-  ['square7', 'square5', 'square3']
 ];
 var currentGame = [];
+var p1 = true;
 var winner = '';
 var player1WinTotal = 0;
 var player2WinTotal = 0;
+var p1Moves = [];
+var p2Moves = [];
 
 //------------ Event Listners ---------------
 window.addEventListener('load', cleanGameBoard);
-gameArea.addEventListener('click', makeMove);
-
-//--------------- Work Area-----------------
-
-
-
-
-
-
+gameSquareBtns.addEventListener('click', makeMove);
 
 //---------------- Functions ----------------
 function cleanGameBoard() {
   currentGame = [];
   newGame = new Game();
   currentGame.push(newGame);
+  resetGlobalVars();
   resetSquares();
+  resetHTML();
+};
+
+function resetGlobalVars() {
+  p1 = currentGame[0].currentPlayer1;
+  p1Moves = currentGame[0].player1Moves;
+  p2Moves = currentGame[0].player2Moves;
+}
+
+function resetSquares() {
+  for (var i = 0; i < gameSquares.length; i++) {
+    gameSquares[i].disabled = false;
+    gameSquares[i].classList.remove('player1', 'player2', 'disabled')
+  };
+};
+
+function resetHTML() {
   player1WinCounter.innerText = `${player1WinTotal}`;
   player2WinCounter.innerText = `${player2WinTotal}`;
+  gameResult.innerText = '';
+  hide(gameResult);
+  show(gameSquareBtns);
 }
 
 function makeMove() {
@@ -65,71 +79,109 @@ function makeMove() {
   markSquares();
   checkWin();
   changePlayer();
-}
+};
 
 function logMove() {
   for (var i = 0; i < gameSquares.length; i++) {
-    if (event.target.id === gameSquares[i].id && currentGame[0].currentPlayer1) {
-      currentGame[0].player1Moves.push(gameSquares[i].id);
-    } else if (event.target.id === gameSquares[i].id && !currentGame[0].currentPlayer1){
-      currentGame[0].player2Moves.push(gameSquares[i].id);
+    if (event.target.id === gameSquares[i].id && p1) {
+      gameSquares[i].disabled = true
+      addToPlayer1(gameSquares[i]);
+    } else if (event.target.id === gameSquares[i].id && !p1) {
+      gameSquares[i].disabled = true
+      addToPlayer2(gameSquares[i]);
     };
   };
 };
 
+function addToPlayer1(move) {
+  move.classList.add('player-1');
+  p1Moves.push(move.id);
+};
+
+function addToPlayer2(move) {
+  move.classList.add('player-2');
+  p2Moves.push(move.id);
+};
+
 function markSquares() {
-  for (var i = 0; i < currentGame[0].player1Moves.length; i++) {
-    var square = document.getElementById(`${currentGame[0].player1Moves[i]}`)
-    square.innerText = 'x';
+  for (var i = 0; i < p1Moves.length; i++) {
+    markPlayer1Square();
   };
-  for (var i = 0; i < currentGame[0].player2Moves.length; i++) {
-    var square = document.getElementById(`${currentGame[0].player2Moves[i]}`)
-    square.innerText = 'o';
+  for (var i = 0; i < p2Moves.length; i++) {
+    markPlayer2Square();
+  };
+};
+
+function markPlayer1Square() {
+  for (var i = 0; i < p1Moves.length; i++) {
+    var square = document.getElementById(`${p1Moves[i]}`);
+    square.classList.add('disabled');
+    square.classList.add('player1');
+  };
+};
+
+function markPlayer2Square() {
+  for (var i = 0; i < p2Moves.length; i++) {
+    var square = document.getElementById(`${p2Moves[i]}`);
+    square.classList.add('disabled');
+    square.classList.add('player2');
   };
 };
 
 function checkWin() {
-  var player1MovesStr = currentGame[0].player1Moves.join();
-  var player2MovesStr = currentGame[0].player2Moves.join();
+  var totalMovesTaken = p1Moves.length + p2Moves.length;
   for (i = 0; i < winningCombos.length; i++) {
-    if (player1MovesStr.includes(winningCombos[i])) {
-      player1Wins();
-    } else if (player2MovesStr.includes(winningCombos[i])) {
-      player2Wins();
+    if (compareArrays(p1Moves, winningCombos[i])) {
+      return player1Wins();
+    } else if (compareArrays(p2Moves, winningCombos[i])) {
+      return player2Wins();
+    } else if (totalMovesTaken === 9) {
+      return declareDraw();
     };
   };
 };
 
-function changePlayer() {
-  if (currentGame[0].currentPlayer1) {
-    currentGame[0].currentPlayer1 = false;
-  } else if (!currentGame[0].currentPlayer1) {
-    currentGame[0].currentPlayer1 = true;
-  };
-  return currentGame[0].currentPlayer1;
+function compareArrays(playerMoves, winCombo) {
+  var matchingMoves = playerMoves.filter((item) => winCombo.includes(item));
+  return matchingMoves.length === 3;
 };
 
 function player1Wins() {
-  console.log('player1 wins');
   player1WinTotal++;
-  console.log(player1WinTotal);
+  announceResult('Player 1');
   resetGame();
 };
 
 function player2Wins() {
-  console.log('player2 wins');
   player2WinTotal++;
+  announceResult('Player 2')
   resetGame();
 };
+
+function announceResult(player) {
+  hide(gameSquareBtns);
+  show(gameResult);
+  gameResult.innerText = `${player} wins!`;
+}
+
+function declareDraw() {
+  hide(gameSquareBtns);
+  show(gameResult);
+  gameResult.innerText = `The cat got the game!`;
+  resetGame();
+}
 
 function resetGame() {
   window.setTimeout(cleanGameBoard, 2500);
 };
 
-function resetSquares() {
-  for (var i = 0; i < gameSquares.length; i++) {
-    gameSquares[i].innerText = '-';
+function changePlayer() {
+  if (p1) {
+    p1 = !p1;
+  } else if (!p1) {
+    p1 = true;
   };
+  return p1;
 };
 
 function hide(element) {
